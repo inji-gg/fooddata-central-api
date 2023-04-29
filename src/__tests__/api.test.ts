b0728f23cfe2a60fa6f75cd0a91cffd023fb636d
arunachaleshwar <5071242+arunachaleshwar@users.noreply.github.com>
2023-04-29 12:25:48 -0700

    snapshot tests for food search

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -3,13 +3,48 @@ dotenv.config();
 
 import tap from "tap";
 import { findFood } from "../api";
-import { DataType, SearchResult } from "../types";
+import { DataType } from "../types";
 
-tap.matchSnapshot(async (): Promise<SearchResult> => {
-  return await findFood({
+tap.test("isSearchMatch_Foundation", (t: Tap.Test) =>
+  findFood({
     query: "milk",
     dataType: [DataType.FOUNDATION],
     pageSize: 1,
     pageNumber: 1,
-  });
-}, "isSearchMatch_Foundation");
+    startDate: "2019-01-01",
+    endDate: "2023-01-01",
+  }).then((result) => t.matchSnapshot(result, "isSearchMatch_Foundation"))
+);
+
+tap.test("isSearchMatch_Branded", (t: Tap.Test) =>
+  findFood({
+    query: "milk",
+    dataType: [DataType.BRANDED],
+    pageSize: 1,
+    pageNumber: 1,
+    startDate: "2019-01-01",
+    endDate: "2023-01-01",
+  }).then((result) => t.matchSnapshot(result, "isSearchMatch_Branded"))
+);
+
+tap.test("isSearchMatch_SurveyFNDDS", (t: Tap.Test) =>
+  findFood({
+    query: "milk",
+    dataType: [DataType.SURVEY_FNDDS],
+    pageSize: 1,
+    pageNumber: 1,
+    startDate: "2019-01-01",
+    endDate: "2023-01-01",
+  }).then((result) => t.matchSnapshot(result, "isSearchMatch_SurveyFNDDS"))
+);
+
+tap.test("isSearchMatch_SRLegacy", (t: Tap.Test) =>
+  findFood({
+    query: "milk",
+    dataType: [DataType.SR_LEGACY],
+    pageSize: 1,
+    pageNumber: 1,
+    startDate: "2019-01-01",
+    endDate: "2023-01-01",
+  }).then((result) => t.matchSnapshot(result, "isSearchMatch_SRLegacy"))
+);
